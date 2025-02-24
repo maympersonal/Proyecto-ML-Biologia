0d52dc971d515f6f323491a5d0156c7c6c3c0cf0
Daniel Ángel <danielarromoreno@gmail.com>
2025-02-23 20:12:03 -0500

    add autoencoder-svm and sift_lbp-random_forest variants

--- src/cbr_model/presets.py
+++ src/cbr_model/presets.py
@@ -2,6 +2,10 @@ from keras.applications import VGG16
 
 from src.cbr_model import CaseBasedReasoning
 from src.cbr_model.feature_extraction import nn_image_to_vector
+from src.cbr_model.feature_extraction_autoencoder import autoencoder_image_to_vector
+from src.cbr_model.feature_extraction_SIFT_LBP import sift_lbp_image_to_vector
+from src.cbr_model.knowledge_managers_svm import svm_manager
+from src.cbr_model.knowledge_managers_random_forest import random_forest_manager
 from src.utils.image_pipeline import resize_and_pad_image
 
 """
@@ -22,5 +26,33 @@ def imagenetCNN_euclideanDistance_knn() -> CaseBasedReasoning:
     cbr.feature_extractor = lambda image: nn_image_to_vector(
         resize_and_pad_image(image), model
     )
+
+    return cbr
+
+
+def imagenetCNN_euclideanDistance_autoencoder_svm() -> CaseBasedReasoning:
+    cbr = CaseBasedReasoning()
+
+    model = VGG16(weights="imagenet", include_top=False, pooling="avg")
+
+    cbr.feature_extractor = lambda image: autoencoder_image_to_vector(
+        resize_and_pad_image(image), model
+    )
+
+    cbr.knowledge_manager = svm_manager
+
+    return cbr
+
+
+def imagenetCNN_euclideanDistance_sift_lbp_random_forest() -> CaseBasedReasoning:
+    cbr = CaseBasedReasoning()
+
+    model = VGG16(weights="imagenet", include_top=False, pooling="avg")
+
+    cbr.feature_extractor = lambda image: sift_lbp_image_to_vector(
+        resize_and_pad_image(image), model
+    )
+
+    cbr.knowledge_manager = random_forest_manager
 
     return cbr
